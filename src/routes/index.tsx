b76f1d2f85b4be606052d8940b07ff30973091b8
Sami Jaber <jabersami@gmail.com>
2023-08-25 13:29:31 -0300

    repro

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,41 +1,74 @@
-import { component$ } from "@builder.io/qwik";
-import ContentComponent from "~/sdk-src/content";
+import { Slot, component$, useStore, Fragment } from "@builder.io/qwik";
 
-export default component$(() => {
+type Columns = Array<string>;
+
+type Content = {
+  blocks: Array<{
+    columns: Columns;
+  }>;
+};
+
+const clone = <T,>(x: T): T => JSON.parse(JSON.stringify(x));
+
+const MODIFIED_COLUMNS: Content = { blocks: [{ columns: ["updated text!"] }] };
+
+export const EnableEditor = component$(
+  (props: { context: { content: Content } }) => {
+    return (
+      <>
+        <button
+          onClick$={() => {
+            const newContentValue = {
+              ...props.context.content,
+              ...MODIFIED_COLUMNS,
+            };
+            props.context.content = newContentValue;
+          }}
+        >
+          update me
+        </button>
+        <Slot></Slot>
+      </>
+    );
+  }
+);
+
+export const Columns = component$((props: { columns: Columns }) => {
+  return <div>in Columns: {props.columns[0]}</div>;
+});
+
+export const Blocks = component$((props: { content: Content }) => {
   return (
-    <ContentComponent
-      content={{
-        data: {
-          title: "Columns",
-          blocks: [
-            {
-              "@type": "@builder.io/sdk:Element",
-              id: "builder-25c64e9c18804f46b73985264df3c41c",
-              component: {
-                name: "Columns",
-                options: {
-                  columns: [
-                    {
-                      blocks: [
-                        {
-                          "@type": "@builder.io/sdk:Element",
-                          "@version": 2,
-                          id: "builder-71c14581f362486eb24214d27c0c24d0",
-                          component: {
-                            name: "Text",
-                            options: { text: "<p>text in column 1</p>" },
-                          },
-                        },
-                      ],
-                    },
-                  ],
-                },
-              },
-            },
-          ],
-        },
-        id: "f24c6940ee5f46458369151cc9ec598c",
-      }}
-    />
+    <>
+      <div>outside loop: {JSON.stringify(props.content.blocks[0].columns)}</div>
+      {props.content.blocks.map((block) => {
+        return (
+          <Fragment key={"render-block-" + block}>
+            {/* uncomment this to fix  */}
+            {/* <div>in BLOCKS (inside loop): {JSON.stringify(block.columns)}</div> */}
+            <Columns columns={block.columns} />
+          </Fragment>
+        );
+      })}
+    </>
   );
 });
+
+export const ContentComponent = component$((props: { content: Content }) => {
+  const state = useStore(
+    { context: { content: clone(props.content) } },
+    { deep: true }
+  );
+
+  return (
+    <EnableEditor context={state.context}>
+      <Blocks content={state.context.content}></Blocks>
+    </EnableEditor>
+  );
+});
+
+export default component$(() => (
+  <ContentComponent
+    content={{ blocks: [{ columns: ["<p>text in column 1</p>"] }] }}
+  />
+));
